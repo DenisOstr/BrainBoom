be323d1293fb4a1855ae0612bf5374d8abc37d23
mrnikituwka <mr.nikituwka@gmail.com>
2019-10-18 12:40:58 +0300

    locate completion

--- local.py
+++ local.py
@@ -9,11 +9,12 @@ menuScreenRu = '''\nВыберите:
 menuSelectNotExistsEng = 'This function does not exists'
 menuSelectNotExistsRu = 'Данной функции не существует'
 
-enterUsernameEng = 'Enter username: '
-enterUsernemeRu = 'Введите имя пользователя: '
-
+# Admin panel
 loginAdminPanelEng = 'Login to Admin panel\n'
 loginAdminPanelRu = 'Вход в Админа панель\n'
+
+enterUsernameEng = 'Enter username: '
+enterUsernemeRu = 'Введите имя пользователя: '
 
 enterPasswordEng = 'Enter password: '
 enterPasswordRu = 'Введите пароль: '
